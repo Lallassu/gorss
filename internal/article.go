c92783e99bccfc0de9e2ea05fc5d426fd2299d89
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-07-14 17:56:30 +1000

    Add feedDisplay to Article

--- internal/article.go
+++ internal/article.go
@@ -6,14 +6,15 @@ import (
 
 // Article holds the content of a feed item
 type Article struct {
-	c         *Controller
-	id        int
-	feed      string
-	title     string
-	content   string
-	link      string
-	read      bool
-	deleted   bool
-	highlight bool
-	published time.Time
+	c           *Controller
+	id          int
+	feed        string
+	feedDisplay string
+	title       string
+	content     string
+	link        string
+	read        bool
+	deleted     bool
+	highlight   bool
+	published   time.Time
 }
