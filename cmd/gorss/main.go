1f6cff16a5e397a15fd99187acadc9bb5ce542f9
Sam Boysel <sboysel@gmail.com>
2022-05-23 14:59:41 -0700

    copy default theme and config to XDG_CONFIG_HOME if they do not exist

--- cmd/gorss/main.go
+++ cmd/gorss/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+        "errors"
 	"flag"
 	"fmt"
 	"log"
@@ -57,6 +58,11 @@ func main() {
 			cfg = s
 		} else {
 			cfg = fmt.Sprintf("%s/%s", configHome, defaultConfig)
+                        // check if default config exists, copy if not
+                        _, err := os.Stat(cfg)
+                        if errors.Is(err, os.ErrNotExist) {
+                            internal.CopyFile(defaultConfig, cfg)
+                        }
 		}
 	}
 
@@ -67,6 +73,15 @@ func main() {
 			theme = s
 		} else {
 			theme = fmt.Sprintf("%s/%s", configHome, defaultTheme)
+                        // check if default config exists, copy if not
+                        _, err := os.Stat(theme)
+                        if errors.Is(err, os.ErrNotExist) {
+                            themeDir := fmt.Sprintf("%s/%s", configHome, "themes")
+                            if err := os.Mkdir(themeDir, os.ModePerm); err != nil {
+                                log.Printf("Failed to create dir: %s\n", themeDir)
+                            }
+                            internal.Copy(defaultTheme, theme)
+                        }
 		}
 	}
 
